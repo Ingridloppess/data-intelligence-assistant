9de308e7422eab08afdf9e52eb007a702cc3d730
Ingrid Lopes <ingridloppess@gmail.com>
2026-09-17 14:36:23 -0300

    feat: adiciona extração de dados da API do Banco Central

--- src/extract/bcb_api.py
+++ src/extract/bcb_api.py
@@ -1,1 +1,35 @@
-print("Conectando à API do Banco Central...")
+import requests
+import pandas as pd
+
+url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json&dataInicial=17/09/2016"
+
+print(url)
+response = requests.get(
+    url,
+    timeout=30
+)
+
+dados = response.json()
+print(type(dados))
+print(dados)
+df = pd.DataFrame(dados)
+
+print(df.head())
+
+# Validações
+print("\nShape:")
+print(df.shape)
+
+print("\nTipos:")
+print(df.dtypes)
+
+print("\nValores nulos:")
+print(df.isnull().sum())
+
+# Salvar CSV
+df.to_csv(
+    "data/raw/selic.csv",
+    index=False
+)
+
+print("\nArquivo salvo com sucesso!")
