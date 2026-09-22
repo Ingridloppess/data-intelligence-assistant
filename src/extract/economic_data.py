46992ec0cef5f2f6994fa8ee55cb740cac75d8c6
Ingrid Lopes <ingridloppess@gmail.com>
2026-09-22 09:49:26 -0300

    feat: cria estrutura reutilizável para indicadores economicos

--- src/extract/economic_data.py
+++ src/extract/economic_data.py
@@ -1,5 +1,25 @@
-def download_indicator(
-    codigo,
-    nome_arquivo
-):
-    pass
+import requests
+import pandas as pd
+
+
+def download_indicator(codigo):
+
+    url = (
+        f"https://api.bcb.gov.br/dados/"
+        f"serie/bcdata.sgs.{codigo}/dados"
+        f"?formato=json"
+        f"&dataInicial=17/09/2016"
+    )
+
+    response = requests.get(
+        url,
+        timeout=30
+    )
+
+    response.raise_for_status()
+
+    dados = response.json()
+
+    df = pd.DataFrame(dados)
+
+    return df
